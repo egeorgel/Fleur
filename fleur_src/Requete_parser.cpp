fa071837ca19cd40ac9adf5231ef6d92e90824ce
egeorgel <edgar.georgel@gmail.com>
2016-11-03 08:20:21 +0100

    split parameters in list of pair

--- fleur_src/Requete_parser.cpp
+++ fleur_src/Requete_parser.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "Requete_parser.h"
+
+/// PUBLIC
+
+/**
+ * string _parametersStr to type_parameterS _parameters
+ */
+void parser::Requete::splitParametersStr() {
+
+    boost::erase_all(_parametersStr, " ");
+
+    std::vector<std::string> parametersSplitByEgual = split(_parametersStr, ',');
+
+    for (auto & element : parametersSplitByEgual) {
+        std::vector<std::string> parameterIvect = split(element, '=');
+        type_parameter parameter = std::make_pair(parameterIvect.front(), parameterIvect.back());
+        _parameters.push_back(parameter);
+    }
+}
+
+/// PRIVATE
+
+/**
+ * Split string by a delimiter
+ * @param s
+ * @param delim
+ * @return
+ */
+std::vector<std::string> parser::Requete::split(const std::string &s, char delim) {
+    std::stringstream stringstream;
+    stringstream.str(s);
+    std::string item;
+
+    std::vector<std::string> parameters;
+    while (std::getline(stringstream, item, delim)) {
+        parameters.push_back(item);
+    }
+    return parameters;
+}
